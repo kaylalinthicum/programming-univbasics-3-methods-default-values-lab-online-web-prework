138d14a82e427950dfa5ad2cec723bfffccb819a
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-11 01:28:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ def meal_choice(veg1, veg2, protein = 'meat')
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
 
-meal_choice("broccoli", "carrots")
+meal_choice("broccoli", "macaroni")
 
-meal_choice("broccoli", "carrots", "chicken")
+meal_choice("broccoli", "macaroni", "tofu")
